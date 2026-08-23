150d65883a7a1656e57965cb960efa79569f7b1f
Akhil Narang <me@akhilnarang.dev>
2026-08-23 23:39:10 +0530

    Vendor protoc for Python package builds

--- build.rs
+++ build.rs
@@ -1,7 +1,11 @@
 fn main() {
-    prost_build::compile_protos(
-        &["src/protos/chromeos_update_engine/update_metadata.proto"],
-        &[] as &[&str],
-    )
-    .expect("error compiling protobuf files");
+    let protoc = protoc_bin_vendored::protoc_bin_path().expect("unable to find vendored protoc");
+    let mut config = prost_build::Config::new();
+    config.protoc_executable(protoc);
+    config
+        .compile_protos(
+            &["src/protos/chromeos_update_engine/update_metadata.proto"],
+            &[] as &[&str],
+        )
+        .expect("error compiling protobuf files");
 }
